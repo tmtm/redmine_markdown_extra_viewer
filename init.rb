d0d7f22d4b086d774c7f163a0f42dedd62e8333f
TOMITA Masahiro <tommy@tmtm.org>
2010-09-17 23:20:13 +0900

    rewrite

--- init.rb
+++ init.rb
@@ -1,22 +1,14 @@
 require 'redmine'
 require 'bluefeather'
 
-module Redmine::SyntaxHighlighting::BlueFeather
-  def self.set_highlighter
-    @orig_highlighter = Redmine::SyntaxHighlighting.highlighter
-    Redmine::SyntaxHighlighting.highlighter = self
-  end
-
-  def self.highlight_by_filename(text, filename)
-    if filename =~ /\.(md|markdown)\z/
-      ::BlueFeather.parse text
-    else
-      @orig_highlighter.highlight_by_filename(text, filename)
+RepositoriesController.class_eval do
+  alias markdown_extra_viewer_orig_entry entry
+  def entry
+    markdown_extra_viewer_orig_entry
+    if not performed? and @path =~ /\.(md|markdown)\z/
+      @content = BlueFeather.parse @content
+      render :action => "entry_markdown"
     end
-  end
-
-  def self.highlight_by_language(text, language)
-    @orig_highlighter.highlight_by_filename(text, filename)
   end
 end
 
@@ -27,7 +19,4 @@ Redmine::Plugin.register :redmine_markdown_extra_viewer do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://github.com/tmtm'
-
-  Redmine::SyntaxHighlighting::BlueFeather.set_highlighter
 end
-
